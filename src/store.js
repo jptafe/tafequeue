c8345c78c247a7407a93c89e00d393916ccc5e6c
developer <developer@localhost.localdomain>
2019-09-25 14:09:49 +1000

    some edits

--- src/store.js
+++ src/store.js
@@ -5,12 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    count: 0
   },
   mutations: {
-
+    increment (state) {
+      state.count++
+    }
   },
   actions: {
-
+    increment (context) {
+      context.commit('increment')
+    }
   }
 })
