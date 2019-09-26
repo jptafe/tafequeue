a5485636c7b1e9e523d534767a5203a5ff074519
jp <jp@localhost.localdomain>
2019-09-26 20:58:19 +1000

    axios test

--- src/store.js
+++ src/store.js
@@ -1,20 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex, axios)
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    data: []
   },
   mutations: {
     increment (state) {
       state.count++
+    },
+    setData (state, gottenData) {
+      state.data = gottenData
     }
   },
   actions: {
     increment (context) {
       context.commit('increment')
+    },
+    getData (state) {
+      axios
+        .get('https://jsonplaceholder.typicode.com/todos/1')
+        .then(data => {
+          console.log(data)
+          this.commit('setData', data.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="goGet()">Hit Me</button>
   </div>
 </template>
+<script>
+
+export default {
+  name: 'about',
+  methods: {
+    goGet () {
+      this.$store.dispatch('getData')
+    }
+  },
+  mounted () {
+    this.$store.dispatch('getData')
+  }
+}
+</script>
