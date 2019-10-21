6b2d83e46824e4ce99a15e697062541a830b7271
Frag Spawn <frag.spawn@gmail.com>
2019-10-21 11:00:13 +1000

    update sql

--- srv/sql/sqs.sql
+++ srv/sql/sqs.sql
@@ -1,108 +1,158 @@
--- MySQL dump 10.17  Distrib 10.3.17-MariaDB, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: sqs
--- ------------------------------------------------------
--- Server version	10.3.17-MariaDB
+-- Host: localhost
+-- Generation Time: Oct 21, 2019 at 12:59 AM
+-- Server version: 10.3.17-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `sqs`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `queue`
 --
 
-DROP TABLE IF EXISTS `queue`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `queue` (
-  `queue_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `queue_ID` int(11) NOT NULL,
   `student_NO` bigint(20) NOT NULL,
   `queue_TITLE` varchar(256) NOT NULL,
   `queue_DESC` varchar(2048) NOT NULL,
-  `queue_DATE` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`queue_ID`),
-  KEY `student_NO` (`student_NO`),
-  CONSTRAINT `queue_ibfk_1` FOREIGN KEY (`student_NO`) REFERENCES `student` (`student_NO`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `queue_DATE` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `queue`
 --
 
-LOCK TABLES `queue` WRITE;
-/*!40000 ALTER TABLE `queue` DISABLE KEYS */;
-INSERT INTO `queue` VALUES (17,456955962,'asdf','asdf','2019-09-28 13:27:04'),(18,460728520,'qwer','qwer','2019-09-28 13:27:04');
-/*!40000 ALTER TABLE `queue` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `queue` (`queue_ID`, `student_NO`, `queue_TITLE`, `queue_DESC`, `queue_DATE`) VALUES
+(17, 456955962, 'asdf', 'asdf', '2019-09-28 13:27:04'),
+(18, 460728520, 'qwer', 'qwer', '2019-09-28 13:27:04');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `student` (
   `student_NO` bigint(20) NOT NULL,
   `nick` varchar(64) NOT NULL,
   `pass` varchar(256) NOT NULL,
-  `privilege` int(4) NOT NULL,
-  PRIMARY KEY (`student_NO`)
+  `privilege` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `student`
 --
 
-LOCK TABLES `student` WRITE;
-/*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (451025951,'','',1),(452759541,'','',1),(452825763,'','',1),(454739343,'','',1),(455111948,'','',1),(456876549,'','',1),(456955962,'','',1),(460728520,'','',1),(465063477,'','',1),(465568673,'','',1),(468299219,'fragspawn','asdfasdf',0),(469403190,'','',1),(470009192,'','',1),(470903074,'','',1),(470949609,'','',1),(470968167,'','',1),(470984545,'','',1),(470985577,'','',1),(470986402,'','',1),(471004279,'','',1),(471017510,'','',1);
-/*!40000 ALTER TABLE `student` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `student` (`student_NO`, `nick`, `pass`, `privilege`) VALUES
+(451025951, '', '', 1),
+(452759541, '', '', 1),
+(452825763, '', '', 1),
+(454739343, '', '', 1),
+(455111948, '', '', 1),
+(456876549, '', '', 1),
+(456955962, '', '', 1),
+(460728520, '', '', 1),
+(465063477, '', '', 1),
+(465568673, '', '', 1),
+(468299219, 'fragspawn', 'asdfasdf', 0),
+(469403190, '', '', 1),
+(470009192, '', '', 1),
+(470903074, '', '', 1),
+(470949609, '', '', 1),
+(470968167, '', '', 1),
+(470984545, '', '', 1),
+(470985577, '', '', 1),
+(470986402, '', '', 1),
+(471004279, '', '', 1),
+(471017510, '', '', 1);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `suggestion`
 --
 
-DROP TABLE IF EXISTS `suggestion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `suggestion` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `sggestion` varchar(1024) NOT NULL,
-  `queue_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `queue_id` (`queue_id`),
-  CONSTRAINT `suggestion_ibfk_1` FOREIGN KEY (`queue_id`) REFERENCES `queue` (`queue_ID`)
+  `queue_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `suggestion`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `suggestion` WRITE;
-/*!40000 ALTER TABLE `suggestion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `suggestion` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `queue`
+--
+ALTER TABLE `queue`
+  ADD PRIMARY KEY (`queue_ID`),
+  ADD KEY `student_NO` (`student_NO`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_NO`);
+
+--
+-- Indexes for table `suggestion`
+--
+ALTER TABLE `suggestion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `queue_id` (`queue_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `queue`
+--
+ALTER TABLE `queue`
+  MODIFY `queue_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `suggestion`
+--
+ALTER TABLE `suggestion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `queue`
+--
+ALTER TABLE `queue`
+  ADD CONSTRAINT `queue_ibfk_1` FOREIGN KEY (`student_NO`) REFERENCES `student` (`student_NO`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `suggestion`
+--
+ALTER TABLE `suggestion`
+  ADD CONSTRAINT `suggestion_ibfk_1` FOREIGN KEY (`queue_id`) REFERENCES `queue` (`queue_ID`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-09-28 13:54:24
